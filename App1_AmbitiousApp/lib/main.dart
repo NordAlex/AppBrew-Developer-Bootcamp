14c478f0a959bd98d3466a19fb7326bc86d3a707
NordAlex <alexhac94@gmail.com>
2019-12-16 23:54:19 +0200

    fix typo

--- App1_AmbitiousApp/lib/main.dart
+++ App1_AmbitiousApp/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'I am abitious'),
+      home: MyHomePage(title: 'I am ambitious'),
     );
   }
 }
